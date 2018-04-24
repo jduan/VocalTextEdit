f17e4060afa7b9b227db412faab431074a06b863
Jingjing Duan <duanjingjing@gmail.com>
2018-04-23 22:36:54 -0700

    new app

---
+++ VocalTextEdit/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  VocalTextEdit
+//
+//  Created by jingjing_duan on 4/23/18.
+//  Copyright © 2018 BigNerdRanch. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ VocalTextEdit/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  VocalTextEdit
+//
+//  Created by jingjing_duan on 4/23/18.
+//  Copyright © 2018 BigNerdRanch. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
